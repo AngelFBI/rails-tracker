cea998b2e6525f07badb90f4ed10697c55d4d4c3
AngelFBI <angel.bejarano@enroutesystems.com>
2022-09-05 16:47:10 -0500

    Added display of Stock info in my portfolio
    - Changed render from json to view in stocks controller
    - Changed return from one info into a hash

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,6 +5,10 @@ class Stock < ApplicationRecord
       secret_token: Rails.application.credentials.iex[:secret_key],
       endpoint: 'https://cloud.iexapis.com/v1'
     )
-    client.quote(ticker_symbol).latest_price
+
+    new(ticker: ticker_symbol,
+      name: client.company(ticker_symbol).company_name,
+      last_price: client.quote(ticker_symbol).latest_price
+    )
   end
 end
