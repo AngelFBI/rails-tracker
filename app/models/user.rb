b9ae8ae1d2fd35df1d508706e3592a6a6a456e3f
AngelFBI <angel.bejarano@enroutesystems.com>
2022-09-06 15:48:37 -0500

    Added Search functionality in frontend
    - Added method except_current_user to exclude the current user from the search method
    - Changed display of list of search users to a table format

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,4 +54,9 @@ class User < ApplicationRecord
   def self.matches(field_name,param)
     where("#{field_name} LIKE ?","%#{param}%")
   end
+
+
+  def except_current_user(users)
+    users.reject { |user| user.id == self.id}
+  end
 end
