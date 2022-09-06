a60ac4fc72041fe9b8b8d1075204ecdd4e9cb61b
AngelFBI <angel.bejarano@enroutesystems.com>
2022-09-06 16:18:46 -0500

    Added add user functionality
    - Added create action to friendships controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,14 @@
 class FriendshipsController < ApplicationController
 
   def create
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = 'Added friend'
+    else
+      flash[:alert] = 'There was something wrong'
+    end
+    redirect_to my_friends_path
   end
 
   def destroy
